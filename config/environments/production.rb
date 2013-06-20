39ac353fc9b55a7f747bbeca66ed1074aab52744
waldix <waldix86@gmail.com>
2013-06-20 18:54:53 -0500

    configuracion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,5 +64,5 @@ Altar::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = { :host => 'http://alt4r.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'alt4r.herokuapp.com' }
 end
